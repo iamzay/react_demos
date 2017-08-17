c0098451a875152aebde5fb95ac4b89ca49e46b1
Unknown <zayhust@gmail.com>
2017-08-17 11:19:59 +0800

    添加初始文件

---
+++ react-hn-front-page/src/App.js
@@ -0,0 +1,4 @@
+/**
+ * Created by zay on 2017/7/17.
+ */
+import React from 'react';
---
+++ react-hn-front-page/src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zay on 2017/7/9.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import App from "./App.js";
+
+render(
+    <div>
+        hello world
+    </div>,
+    document.getElementById('root')
+);
